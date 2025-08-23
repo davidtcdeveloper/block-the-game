1365ef76a76c5599ee7dad19679f3e870af19ee4
David Tiago Conceição <davidtc.developer@gmail.com>
2025-08-23 21:49:30 +0000

    Compile fixes

--- app/src/main/java/com/quantumblocks/game/di/AppModule.kt
+++ app/src/main/java/com/quantumblocks/game/di/AppModule.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
--- app/src/main/java/com/quantumblocks/game/ui/theme/Theme.kt
+++ app/src/main/java/com/quantumblocks/game/ui/theme/Theme.kt
@@ -46,8 +46,7 @@ fun QuantumBlocksTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
